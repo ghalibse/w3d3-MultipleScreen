53df7cb2490249118237091ff73be8ba06e19cde
ghalibse <ghalibse@gmail.com>
2016-08-04 00:57:07 -0400

    After LandScape

--- app/src/main/java/com/example/simplelist/MainActivity.java
+++ app/src/main/java/com/example/simplelist/MainActivity.java
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import com.squareup.picasso.Picasso;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -25,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     private String dataJSON = "[{\"name\":\"Juan\",\"age\":20,\"grade\":8.1},{\"name\":\"Miguel\",\"age\":23,\"grade\":8.3},{\"name\":\"Roberto\",\"age\":39,\"grade\":9.3},{\"name\":\"Luis\",\"age\":19,\"grade\":6.9},{\"name\":\"Gaudencio\",\"age\":25,\"grade\":4.3}]";
+    private String[] url = new String[]{"https://goo.gl/iXJNuf", "http://goo.gl/dPs4IF", "http://goo.gl/N6CfR0","https://goo.gl/NqJtxe", "http://goo.gl/aGM2Qq"};
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mListView = (ListView) findViewById(R.id.lst_view);
+        img = (ImageView) findViewById(R.id.img_pic);
 
         Type listType = new TypeToken<List<Student>>() {
         }.getType();
@@ -48,9 +53,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Student aStudent =  arrayAdapter.getItem(i);
-
                 Toast.makeText(getApplicationContext(), aStudent.name.toUpperCase(), Toast.LENGTH_SHORT).show();
 
+                if(img != null)
+                    Picasso.with(getApplicationContext()).load(url[i]).into(img);
             }
         });
     }
